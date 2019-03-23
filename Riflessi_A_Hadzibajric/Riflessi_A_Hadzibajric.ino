268ad5b2c612d883f07d99a3d5d513e8d7c4ff9b
Amer <amer.amer9211@gmail.com>
2019-03-23 08:09:19 +0100

    Caricamento primo file finale

---
+++ Riflessi_A_Hadzibajric/Riflessi_A_Hadzibajric.ino
@@ -0,0 +1,25 @@
+int bottone = 12;
+int buzzer = 4;
+int rosso = 8;
+int verde = 7;
+int blu = 2;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(blu,OUTPUT);
+  pinMode(rosso,OUTPUT);
+  pinMode(bottone, INPUT);
+  pinMode(verde,OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+  
