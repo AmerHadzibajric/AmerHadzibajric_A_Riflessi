093ae4f948c56ce03d8ece94742a8110b66e24c0
Amer <amer.amer9211@gmail.com>
2019-03-23 08:58:59 +0100

    TempoBuzzer completato

--- Riflessi_A_Hadzibajric/Riflessi_A_Hadzibajric.ino
+++ Riflessi_A_Hadzibajric/Riflessi_A_Hadzibajric.ino
@@ -1,7 +1,7 @@
 int bottone = 12;
 int buzzer = 2;
-int rosso = 8;
-int verde = 7;
+int verde = 8;
+int rosso = 7;
 int blu = 4;
 int TempoBuzzer = 0;
 int TempoLed = 0;
@@ -19,6 +19,8 @@ void loop() {
   // put your main code here, to run repeatedly:
   if(digitalRead(bottone) == HIGH)
   {
+    digitalWrite(rosso,LOW);
+    digitalWrite(verde,LOW);
     TempoLed = 0;
     delay(random(1000,5000));
     digitalWrite(blu, HIGH);
@@ -28,8 +30,29 @@ void loop() {
       delay(1);
     }  
     digitalWrite(blu, LOW);
-  Serial.println(TempoLed);    
+  Serial.println(TempoLed); 
+  delay(random(1000,5000));
+  
+    TempoBuzzer = 0;
+    digitalWrite(buzzer, HIGH);
+    while(digitalRead(bottone) == LOW)
+    {
+      TempoBuzzer ++;
+      delay(1);
+    }  
+    digitalWrite(buzzer, LOW);
+  Serial.println(TempoBuzzer);   
+  
+  if(TempoBuzzer > 350)
+  {
+    digitalWrite(rosso,HIGH);
   }
+  else
+  {
+    digitalWrite(verde,HIGH);
+  }
+  delay(500);
+}
 }
 
   
